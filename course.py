106ceb2a5209458e2ed15eb79c76a24e6f351bc7
ubaifadhli <ubaifadhli@it.student.pens.ac.id>
2020-05-10 04:43:12 +0700

    changed the day name in course.py from bahasa indonesia to english.

--- course.py
+++ course.py
@@ -15,22 +15,22 @@ class Course:
 ##
 ##        while i < len(self.timeList):
 ##            time = self.timeList[i]
-##            
+##
 ##            if i == 1: string = string + str(" (")
 ##
 ##            string += str(self.getDayName(time.day)) + str(" ") + str(time.time)
 ##
 ##            i += 1
-##            
+##
 ##            if i != len(self.timeList): string += str(", ")
 
         return string
 
     def getDayName(self, dayNumber):
-        if dayNumber == 1: return "Senin"
-        elif dayNumber == 2: return "Selasa"
-        elif dayNumber == 3: return "Rabu"
-        elif dayNumber == 4: return "Kamis"
-        elif dayNumber == 5: return "Jumat"
-        elif dayNumber == 6: return "Sabtu"
-        elif dayNumber == 7: return "Minggu"
+        if dayNumber == 1: return "Monday"
+        elif dayNumber == 2: return "Tuesday"
+        elif dayNumber == 3: return "Wednesday"
+        elif dayNumber == 4: return "Thursday"
+        elif dayNumber == 5: return "Friday"
+        elif dayNumber == 6: return "Saturday"
+        elif dayNumber == 7: return "Sunday"
